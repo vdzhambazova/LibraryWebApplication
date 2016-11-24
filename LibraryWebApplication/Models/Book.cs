e70917f6c664d5eebdbc8a6f8f1311080eac6d0f
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-11-24 21:04:23 +0200

    adding models classes for books and users; adding db context

---
+++ LibraryWebApplication/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LibraryWebApplication.Models
+{
+    public class Book
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MinLength(2), MaxLength(60), Required]
+        public string Name { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public string Author { get; set; }
+
+        [Required]
+        public byte[] CoverPicture { get; set; }
+
+        [Required, MaxLength(1024*1024)]
+        public Genre Genre { get; set; }
+    }
+
+    public enum Genre
+    {
+        Fiction,
+        Satire,
+        Drama,
+        Adventure,
+        Romance,
+        Mystery,
+        Horror
+    }
+}
---
+++ LibraryWebApplication/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using LibraryWebApplication.Attributes;
+
+namespace LibraryWebApplication.Models
+{
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MinLength(5), MaxLength(20), Required]
+        public string Username { get; set; }
+
+        [Required, Password(6, 50, ShouldContainLowercase = true, ShouldContainDigit = true, ShouldContainSpecialSymbol = false, ShouldContainUppercase = true)]
+        public string Password { get; set; }
+
+    }
+}
